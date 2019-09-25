8df61127dce886fd8ed95049118e1f95bc2adfb7
Dominic <dominic.egginton@gmail.com>
2019-09-25 09:24:03 +0100

    added view controllers

---
+++ Stack/SplitViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SplitViewController.swift
+//  Stack
+//
+//  Created by Dominic Egginton on 25/09/2019.
+//  Copyright © 2019 Dominic Egginton. All rights reserved.
+//
+
+import UIKit
+
+class SplitViewController: UISplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
